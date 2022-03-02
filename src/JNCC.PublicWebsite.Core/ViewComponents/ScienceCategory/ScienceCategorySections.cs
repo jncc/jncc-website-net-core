db27b1ba00999edc45035b2d1e262b8dc40a249f
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 13:28:26 +0000

    Adding Sections ViewComponent and Partial View

---
+++ src/JNCC.PublicWebsite.Core/ViewComponents/ScienceCategory/ScienceCategorySections.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using JNCC.PublicWebsite.Core.Interfaces.Services;
+using JNCC.PublicWebsite.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace JNCC.PublicWebsite.Core.ViewComponents
+{
+    [ViewComponent(Name = "ScienceCategorySections")]
+    public class ScienceCategorySectionsViewComponent : ViewComponent
+    {
+        private readonly IScienceCategoryPageService _scienceCategoryPageService;
+        public ScienceCategorySectionsViewComponent(IScienceCategoryPageService scienceCategoryPageService)
+        {
+            _scienceCategoryPageService = scienceCategoryPageService ?? throw new ArgumentNullException(nameof(scienceCategoryPageService));
+        }
+
+        public IViewComponentResult Invoke(IPublishedContent model)
+        {
+            var scienceCategory = model as ScienceCategoryPage;
+            var viewModel = _scienceCategoryPageService.GetSectionViewModels(scienceCategory.MainContent);
+
+            return View("~/Views/Partials/ScienceCategory/Sections.cshtml", viewModel);
+        }
+    }
+}
