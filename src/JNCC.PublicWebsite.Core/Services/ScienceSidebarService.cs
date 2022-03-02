5a8ac901b62eef73fd4f9e793db970145af8cc65
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 13:33:32 +0000

    Adding ScienceLandingPage Service and missing services

---
+++ src/JNCC.PublicWebsite.Core/Services/ScienceSidebarService.cs
@@ -0,0 +1,88 @@
+﻿using JNCC.PublicWebsite.Core.Interfaces.Providers;
+using JNCC.PublicWebsite.Core.Interfaces.Services;
+using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Utilities;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using Umbraco.Core;
+using Umbraco.Extensions;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class ScienceSidebarService : SidebarServiceBase, IScienceSidebarService
+    {
+        private readonly ISciencePageCategoriesProvider _sciencePageCategoriesProvider;
+
+        public ScienceSidebarService(INavigationItemService navigationItemService
+            ,IDataHubRawQueryService dataHubRawQueryService
+            , ISciencePageCategoriesProvider sciencePageCategoriesProvider) 
+            : base(navigationItemService
+                  , dataHubRawQueryService
+                  )
+        {
+            _sciencePageCategoriesProvider = sciencePageCategoriesProvider ?? throw new ArgumentNullException(nameof(sciencePageCategoriesProvider));
+        }
+
+        public ScienceSidebarViewModel GetSidebarViewModel(ScienceDetailsPage model)
+        {
+            var viewModel = CreateViewModel<ScienceSidebarViewModel>(model);
+            viewModel.Categories = GetCategoriesWithFeaturedPages(model);
+
+            return viewModel;
+        }
+
+        public ScienceSidebarViewModel GetSidebarViewModel(ScienceCategoryPage model)
+        {
+            var viewModel = CreateViewModel<ScienceSidebarViewModel>(model);
+            viewModel.Categories = GetCategoriesWithFeaturedPages(model);
+
+            return viewModel;
+        }
+
+        public SidebarViewModel GetSidebarViewModel(ScienceAtoZpage model)
+        {
+            return CreateViewModel<SidebarViewModel>(model);
+        }
+
+        private IEnumerable<MainNavigationItemViewModel> GetCategoriesWithFeaturedPages(ScienceDetailsPage model)
+        {
+            var categories = _sciencePageCategoriesProvider.GetCategories(model);
+
+            return GetCategoriesWithFeaturedPages(categories);
+        }
+
+        private IEnumerable<MainNavigationItemViewModel> GetCategoriesWithFeaturedPages(ScienceCategoryPage model)
+        {
+            var category = GetCategoryWithFeaturedPages(model);
+
+            return category.AsEnumerableOfOne();
+        }
+
+        private IEnumerable<MainNavigationItemViewModel> GetCategoriesWithFeaturedPages(IEnumerable<ScienceCategoryPage> categories)
+        {
+            if (ExistenceUtility.IsNullOrEmpty(categories))
+            {
+                return null;
+            }
+
+            var viewModels = new List<MainNavigationItemViewModel>();
+
+            foreach (var category in categories)
+            {
+                var viewModel = GetCategoryWithFeaturedPages(category);
+                viewModels.Add(viewModel);
+            }
+
+            return viewModels;
+        }
+
+        private MainNavigationItemViewModel GetCategoryWithFeaturedPages(ScienceCategoryPage category)
+        {
+            var viewModel = _navigationItemService.GetViewModel<MainNavigationItemViewModel>(category);
+            viewModel.Children = _navigationItemService.GetViewModels<MainNavigationItemViewModel>(category.FeaturedPages);
+
+            return viewModel;
+        }
+    }
+}
---
+++ src/JNCC.PublicWebsite.Core/Services/SidebarServiceBase.cs
@@ -0,0 +1,63 @@
+﻿using JNCC.PublicWebsite.Core.Constants;
+using JNCC.PublicWebsite.Core.Interfaces.Services;
+using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Utilities;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal abstract class SidebarServiceBase
+    {
+        private const int _maxNumberOfResults = 5;
+        protected readonly INavigationItemService _navigationItemService;
+        protected readonly IDataHubRawQueryService _dataHubRawQueryService;
+
+        public SidebarServiceBase(INavigationItemService navigationItemService
+            , IDataHubRawQueryService dataHubRawQueryService
+            )
+        {
+            _navigationItemService = navigationItemService;
+            _dataHubRawQueryService = dataHubRawQueryService;
+        }
+
+        protected T CreateViewModel<T>(ISidebarComposition composition) where T : BasicSidebarViewModel, new()
+        {
+            return new T
+            {
+                PrimaryCallToActionButton = _navigationItemService.GetViewModel(composition.SidebarPrimaryCallToActionButton),
+                DataHubLinks = GetDataHubLinks(composition),
+                SeeAlsoLinks = _navigationItemService.GetViewModels(composition.SidebarSeeAlsoLinks)
+            };
+        }
+
+        private IEnumerable<NavigationItemViewModel> GetDataHubLinks(ISidebarComposition composition)
+        {
+            var viewModels = new List<NavigationItemViewModel>();
+            if (string.IsNullOrWhiteSpace(composition.SidebarDataHubQuery))
+            {
+                return viewModels;
+            }
+
+            var results = _dataHubRawQueryService.GetByRawQuery(composition.SidebarDataHubQuery, _maxNumberOfResults);
+            if (ExistenceUtility.IsNullOrEmpty(results.Hits.Results))
+            {
+                return viewModels;
+            }
+
+            foreach (var result in results.Hits.Results)
+            {
+                var viewModel = new NavigationItemViewModel()
+                {
+                    Text = result.Source.Title,
+                    Url = result.Source.Url,
+                    Target = HtmlAnchorTargets.Blank
+                };
+
+                viewModels.Add(viewModel);
+            }
+
+            return viewModels;
+        }
+    }
+}
