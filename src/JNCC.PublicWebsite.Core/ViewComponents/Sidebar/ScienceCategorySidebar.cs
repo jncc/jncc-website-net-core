15d9ad21f9a13f99daf9b8ea47d2628f97b227de
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 13:30:45 +0000

    Adding Science Category Sidebar ViewComponent

---
+++ src/JNCC.PublicWebsite.Core/ViewComponents/Sidebar/ScienceCategorySidebar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using JNCC.PublicWebsite.Core.Interfaces.Services;
+using JNCC.PublicWebsite.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Extensions;
+
+namespace JNCC.PublicWebsite.Core.ViewComponents
+{
+    [ViewComponent(Name = "ScienceCategoryPageSidebar")]
+    public class ScienceCategoryPageSidebarViewComponent : ViewComponent
+    {
+        private readonly IScienceSidebarService _scienceSidebarService;
+
+        public ScienceCategoryPageSidebarViewComponent(IScienceSidebarService scienceSidebarService)
+        {
+            _scienceSidebarService = scienceSidebarService ?? throw new ArgumentNullException(nameof(scienceSidebarService));
+        }
+        public IViewComponentResult Invoke(IPublishedContent model)
+        {
+            if (model is ScienceCategoryPage == false)
+            {
+                return null;
+            }
+
+            var viewModel = _scienceSidebarService.GetSidebarViewModel(model as ScienceCategoryPage);
+
+            return View("~/Views/Partials/ScienceSidebar.cshtml", viewModel);
+        }
+    }
+}
