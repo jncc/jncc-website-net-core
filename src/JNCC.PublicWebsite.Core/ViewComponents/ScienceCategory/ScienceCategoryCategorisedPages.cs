0422935f35f7726cd53d957dc4761e9bed20e623
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 13:29:59 +0000

    Adding CategorisedPages ViewComponent and Partial View

---
+++ src/JNCC.PublicWebsite.Core/ViewComponents/ScienceCategory/ScienceCategoryCategorisedPages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using JNCC.PublicWebsite.Core.Extensions;
+using JNCC.PublicWebsite.Core.Interfaces.Services;
+using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace JNCC.PublicWebsite.Core.ViewComponents
+{
+    [ViewComponent(Name = "ScienceCategoryCategorisedPages")]
+    public class ScienceCategoryCategorisedPagesViewComponent : ViewComponent
+    {
+        private readonly IScienceCategoryPageService _scienceCategoryPageService;
+        public ScienceCategoryCategorisedPagesViewComponent(IScienceCategoryPageService scienceCategoryPageService)
+        {
+            _scienceCategoryPageService = scienceCategoryPageService ?? throw new ArgumentNullException(nameof(scienceCategoryPageService));
+        }
+
+        public IViewComponentResult Invoke(IPublishedContent model)
+        {
+            var viewModel = new CategorisedPagesViewModel()
+            {
+                Heading = model.GetHeadline(),
+                Pages = _scienceCategoryPageService.GetCategorisedPages(model as ScienceCategoryPage)
+            };
+
+            return View("~/Views/Partials/ScienceCategory/CategorisedPages.cshtml", viewModel);
+        }
+    }
+}
