b832f52becc4a4fe55ef48bcedba29d6e5a90f54
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 11:28:38 +0000

    Adding providers to webcomposer for DI

---
+++ src/JNCC.PublicWebsite.Core/Composer/WebComposer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using JNCC.PublicWebsite.Core.Interfaces.Providers;
+using JNCC.PublicWebsite.Core.Providers;
+
+namespace JNCC.PublicWebsite.Core.Composers
+{
+    public class WebComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            //Services
+            builder.Services.AddSingleton<IScienceDetailsPageProvider, UmbracoScienceDetailsPageProvider>();
+            builder.Services.AddSingleton<ISciencePageCategoriesProvider, UmbracoSciencePageCategoriesProvider>();
+            builder.Services.AddSingleton<IUmbracoArticleYearsProvider, UmbracoArticleYearsProvider>();
+            builder.Services.AddSingleton<IUmbracoArticleTypesProvider, UmbracoArticleTypesProvider>();
+            builder.Services.AddSingleton<IUmbracoArticlePageTagsProvider, UmbracoArticlePageTagsProvider>();
+        }
+    }
+}
