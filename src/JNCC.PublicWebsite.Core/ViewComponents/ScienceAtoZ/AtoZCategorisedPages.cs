19745294bf1d7003f8f3679773c0a5d7a832e834
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 13:42:46 +0000

    Adding CategorisedPages ViewComponent

---
+++ src/JNCC.PublicWebsite.Core/ViewComponents/ScienceAtoZ/AtoZCategorisedPages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using JNCC.PublicWebsite.Core.Interfaces.Services;
+using JNCC.PublicWebsite.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace JNCC.PublicWebsite.Core.ViewComponents
+{
+    [ViewComponent(Name = "AtoZCategorisedPages")]
+    public class AtoZCategorisedPagesViewComponent : ViewComponent
+    {
+        private readonly IScienceAtoZPageService _scienceAtoZPageService;
+        public AtoZCategorisedPagesViewComponent(IScienceAtoZPageService scienceAtoZPageService)
+        {
+            _scienceAtoZPageService = scienceAtoZPageService ?? throw new ArgumentNullException(nameof(scienceAtoZPageService));
+        }
+
+        public IViewComponentResult Invoke(IPublishedContent model)
+        {
+            var viewModel = _scienceAtoZPageService.GetCategorisedPages(model as ScienceAtoZpage);
+
+            return View("~/Views/Partials/Science/CategorisedPages.cshtml", viewModel);
+        }
+    }
+}
