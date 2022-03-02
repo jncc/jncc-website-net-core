e8ab1588a4684bcd6cce83f4ddd481870f0e5994
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 13:42:12 +0000

    Adding ScienceAtoZ Page Service

---
+++ src/JNCC.PublicWebsite.Core/Services/ScienceAtoZPageService.cs
@@ -0,0 +1,31 @@
+﻿using JNCC.PublicWebsite.Core.Extensions;
+using JNCC.PublicWebsite.Core.Interfaces.Services;
+using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Utilities;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Collections.Generic;
+using Umbraco.Extensions;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class ScienceAtoZPageService : IScienceAtoZPageService
+    {
+        public IReadOnlyDictionary<char, IEnumerable<NavigationItemViewModel>> GetCategorisedPages(ScienceAtoZpage model)
+        {
+            var scienceLandingPage = model.Parent<ScienceLandingPage>();
+
+            if (scienceLandingPage == null)
+            {
+                return null;
+            }
+
+            var categorisablePages = scienceLandingPage.Children<IScienceCategorisablePage>();
+            if (ExistenceUtility.IsNullOrEmpty(categorisablePages))
+            {
+                return null;
+            }
+
+            return categorisablePages.CategorisePages();
+        }
+    }
+}
