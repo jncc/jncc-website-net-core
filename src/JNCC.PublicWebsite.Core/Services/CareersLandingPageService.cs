45eba4122970982055df9daa804e6cae21c9e41e
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 13:45:05 +0000

    Adding CareersLanding Service

---
+++ src/JNCC.PublicWebsite.Core/Services/CareersLandingPageService.cs
@@ -0,0 +1,41 @@
+﻿using JNCC.PublicWebsite.Core.Extensions;
+using JNCC.PublicWebsite.Core.Interfaces.Services;
+using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Extensions;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class CareersLandingPageService : ICareersLandingPageService
+    {
+        public IEnumerable<CareersListItemViewModel> GetCareers(CareersLandingPage model)
+        {
+            var jobs = model.Children<IndividualJobPage>();
+            var viewModels = new List<CareersListItemViewModel>();
+
+            if (jobs.Any() == false)
+            {
+                return viewModels;
+            }
+
+            foreach (var job in jobs.OrderBy(x => x.SortOrder))
+            {
+                var viewModel = new CareersListItemViewModel()
+                {
+                    Grade = job.Grade,
+                    JobTitle = job.GetHeadline(),
+                    Location = job.Location,
+                    Team = job.Team,
+                    Type = job.TypeOfAppointment,
+                    Url = job.Url()
+                };
+
+                viewModels.Add(viewModel);
+            }
+
+            return viewModels;
+        }
+    }
+}
