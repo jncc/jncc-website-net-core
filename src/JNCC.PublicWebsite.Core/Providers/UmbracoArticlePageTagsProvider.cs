85f76d2388ab54444ab15faa3ac76d59cfd8fc97
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 11:26:21 +0000

    Adding umbraco providers

---
+++ src/JNCC.PublicWebsite.Core/Providers/UmbracoArticlePageTagsProvider.cs
@@ -0,0 +1,36 @@
+﻿using JNCC.PublicWebsite.Core.Constants;
+using JNCC.PublicWebsite.Core.Interfaces.Providers;
+using JNCC.PublicWebsite.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace JNCC.PublicWebsite.Core.Providers
+{
+    internal sealed class UmbracoArticlePageTagsProvider : UmbracoPagesProvider<ArticlePage>, 
+        IUmbracoArticlePageTagsProvider
+    {
+        private readonly AppCaches _appCaches;
+        public UmbracoArticlePageTagsProvider(AppCaches appCaches): base(appCaches)
+        {
+            _appCaches = appCaches ?? throw new ArgumentNullException(nameof(appCaches));
+        }
+
+        public IEnumerable<string> GetTagsByRoot(IPublishedContent root, string tagGroup)
+        {
+            var articlePages = GetContentPages(root);
+
+            switch (tagGroup)
+            {
+                case TagGroups.Teams:
+                    return articlePages.SelectMany(x => x.ArticleTeams)
+                                       .Distinct()
+                                       .OrderBy(x => x);
+                default:
+                    return Enumerable.Empty<string>();
+            }
+        }
+    }
+}
---
+++ src/JNCC.PublicWebsite.Core/Providers/UmbracoArticleYearsProvider.cs
@@ -0,0 +1,42 @@
+﻿using JNCC.PublicWebsite.Core.Interfaces.Providers;
+using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace JNCC.PublicWebsite.Core.Providers
+{
+    internal sealed class UmbracoArticleYearsProvider : UmbracoPagesProvider<ArticlePage>, IUmbracoArticleYearsProvider
+    {
+        private readonly AppCaches _appCaches;
+        public UmbracoArticleYearsProvider(AppCaches appCaches) : base(appCaches)
+        {
+            _appCaches = appCaches ?? throw new ArgumentNullException(nameof(appCaches));
+        }
+
+        private IEnumerable<int> GetArticleYears(IPublishedContent root)
+        {
+            var articlePages = GetContentPages(root);
+
+            if (ExistenceUtility.IsNullOrEmpty(articlePages))
+            {
+                return Enumerable.Empty<int>();
+            }
+
+            return articlePages.Select(x => x.PublishDate.Year).Distinct();
+        }
+
+        public IEnumerable<int> GetAllByRoot(IPublishedContent root)
+        {
+            return GetArticleYears(root).OrderBy(x => x);
+        }
+
+        public IEnumerable<int> GetAllByRootDescending(IPublishedContent root)
+        {
+            return GetArticleYears(root).OrderByDescending(x => x);
+        }
+    }
+}
