baf39eb881211c535614da7b0582f9700c9ab116
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 11:25:08 +0000

    Adding custom models

---
+++ src/JNCC.PublicWebsite.Core/Models/Custom/FilteringModel.cs
@@ -0,0 +1,21 @@
+﻿namespace JNCC.PublicWebsite.Core.Models
+{
+    public class FilteringModel
+    {
+        public FilteringModel()
+        {
+            PageNumber = 1;
+        }
+
+        public int PageNumber { get; set; }
+    }
+
+    internal interface ITeamsFiltering
+    {
+        string[] Teams { get; set; }
+    }
+    internal interface ISearchTermFiltering
+    {
+        string SearchTerm { get; set; }
+    }
+}
---
+++ src/JNCC.PublicWebsite.Core/Models/Custom/NewsAndInsightsLandingFilteringModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JNCC.PublicWebsite.Core.Models
+{
+    public sealed class NewsAndInsightsLandingFilteringModel : FilteringModel, ISearchTermFiltering, ITeamsFiltering
+    {
+        public string SearchTerm { get; set; }
+        public string[] Teams { get; set; }
+        public string[] ArticleTypes { get; set; }
+        public int[] Years { get; set; }
+    }
+}
