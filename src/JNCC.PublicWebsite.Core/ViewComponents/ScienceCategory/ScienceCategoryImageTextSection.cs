a819e42f317924a69a7ed3c4d162fb6df6eaa06e
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 13:27:58 +0000

    Adding Image and Text Section ViewComponent and Partial View

---
+++ src/JNCC.PublicWebsite.Core/ViewComponents/ScienceCategory/ScienceCategoryImageTextSection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using JNCC.PublicWebsite.Core.Interfaces.Services;
+using JNCC.PublicWebsite.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace JNCC.PublicWebsite.Core.ViewComponents
+{
+    [ViewComponent(Name = "ScienceCategoryImageTextSection")]
+    public class ScienceCategoryImageTextSectionViewComponent : ViewComponent
+    {
+        private readonly IScienceCategoryPageService _scienceCategoryPageService;
+        public ScienceCategoryImageTextSectionViewComponent(IScienceCategoryPageService scienceCategoryPageService)
+        {
+            _scienceCategoryPageService = scienceCategoryPageService ?? throw new ArgumentNullException(nameof(scienceCategoryPageService));
+        }
+
+        public IViewComponentResult Invoke(IPublishedContent model)
+        {
+            var scienceCategory = model as ScienceCategoryPage;
+            var viewModel = _scienceCategoryPageService.GetImageTextSectionViewModels(scienceCategory.ImageAndTextSection);
+
+            return View("~/Views/Partials/ScienceCategory/ImageAndTextSection.cshtml", viewModel);
+        }
+    }
+}
