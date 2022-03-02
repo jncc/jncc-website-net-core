b93e7f3e81498de1a8b0da633615146a96d1ee03
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 13:33:48 +0000

    Adding ScienceLanding Interface

---
+++ src/JNCC.PublicWebsite.Core/Interfaces/Services/IScienceLandingPageService.cs
@@ -0,0 +1,12 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.Interfaces.Services
+{
+    public interface IScienceLandingPageService
+    {
+        ScienceLatestUpdatesSectionViewModel GetLatestUpdates(ScienceLandingPage model);
+        IEnumerable<ResourcesCollectionViewModel> GetResourcesCollections(ScienceLandingPage model);
+    }
+}
