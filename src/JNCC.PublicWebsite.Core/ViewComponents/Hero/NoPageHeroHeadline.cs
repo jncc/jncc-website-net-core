e06aaf8289ad66c1e595d0c53dd32dacd21cec87
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 12:30:35 +0000

    Adding Hero ViewComponents and Partial Views

---
+++ src/JNCC.PublicWebsite.Core/ViewComponents/Hero/NoPageHeroHeadline.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using JNCC.PublicWebsite.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace JNCC.PublicWebsite.Core.ViewComponents
+{
+    [ViewComponent(Name = "NoPageHeroHeadline")]
+    public class NoPageHeroHeadlineViewComponent : ViewComponent
+    {
+        private readonly IPageHeroService _pageHeroService;
+
+        public NoPageHeroHeadlineViewComponent(IPageHeroService pageHeroService)
+        {
+            _pageHeroService = pageHeroService ?? throw new ArgumentNullException(nameof(pageHeroService));
+        }
+        public IViewComponentResult Invoke(IPublishedContent model)
+        {
+            var viewModel = _pageHeroService.RenderNoPageHeroHeadline(model);
+
+            return View("~/Views/Partials/NoPageHeroHeadline.cshtml", viewModel);
+        }
+    }
+}
