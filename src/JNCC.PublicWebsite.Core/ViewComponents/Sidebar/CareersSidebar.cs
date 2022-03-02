3b5b8102c69e96219d1df3a59a370bd1a2382258
Karl Butler <kbutler@carbonsix.digital>
2022-03-02 13:45:44 +0000

    Adding CareersSidebar ViewComponent

---
+++ src/JNCC.PublicWebsite.Core/ViewComponents/Sidebar/CareersSidebar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using JNCC.PublicWebsite.Core.Interfaces.Services;
+using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Extensions;
+
+namespace JNCC.PublicWebsite.Core.ViewComponents
+{
+    [ViewComponent(Name = "CareersSidebar")]
+    public class CareersSidebarViewComponent : ViewComponent
+    {
+        private readonly ICareersSidebarService _careersSidebarService;
+
+        public CareersSidebarViewComponent(ICareersSidebarService careersSidebarService)
+        {
+            _careersSidebarService = careersSidebarService ?? throw new ArgumentNullException(nameof(careersSidebarService));
+        }
+        public IViewComponentResult Invoke(IPublishedContent model)
+        {
+            if (model is CareersLandingPage == false)
+            {
+                return null;
+            }
+
+            var viewModel = _careersSidebarService.GetViewModel(model as CareersLandingPage);
+            return View("~/Views/Partials/CareersSidebar.cshtml", viewModel);
+        }
+    }
+}
