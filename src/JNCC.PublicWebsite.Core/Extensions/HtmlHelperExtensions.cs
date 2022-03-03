d2efb7b8d13d8abcbeb31402d11238c9286b3649
Karl Butler <kbutler@carbonsix.digital>
2022-03-03 08:44:25 +0000

    Updating Helper to get HTMLDirection

--- src/JNCC.PublicWebsite.Core/Extensions/HtmlHelperExtensions.cs
+++ src/JNCC.PublicWebsite.Core/Extensions/HtmlHelperExtensions.cs
@@ -1,21 +1,19 @@
 ﻿using JNCC.PublicWebsite.Core.Constants;
-using Microsoft.AspNetCore.Html;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using System.Globalization;
 namespace JNCC.PublicWebsite.Core.Extensions
 {
     public static class HtmlHelperExtensions
     {
-        //public static string Directionality(this HtmlHelper htmlHelper, CultureInfo cultureInfo)
-        //{
-        //    if (cultureInfo == null || cultureInfo.TextInfo == null)
-        //    {
-        //        return HtmlTextDirectionalities.Auto;
-        //    }
+        public static string Directionality(this HtmlHelper htmlHelper, CultureInfo cultureInfo)
+        {
+            if (cultureInfo == null || cultureInfo.TextInfo == null)
+            {
+                return HtmlTextDirectionalities.Auto;
+            }
 
-        //    return cultureInfo.TextInfo.IsRightToLeft ? HtmlTextDirectionalities.RightToLeft : HtmlTextDirectionalities.LeftToRight;
-        //}
+            return cultureInfo.TextInfo.IsRightToLeft ? HtmlTextDirectionalities.RightToLeft : HtmlTextDirectionalities.LeftToRight;
+        }
 
         //public static IHtmlString If(this HtmlHelper htmlHelper, bool condition, object value)
         //{
