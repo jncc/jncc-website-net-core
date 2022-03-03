aa66f5789bdf8d6241e68c6f9e7fd4e1fa47f279
Karl Butler <kbutler@carbonsix.digital>
2022-03-03 08:43:13 +0000

    Adding page attributes and SEOMetadata tags

---
+++ src/JNCC.PublicWebsite.Core/Interfaces/Services/IPageIncludesService.cs
@@ -0,0 +1,15 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+
+namespace JNCC.PublicWebsite.Core.Interfaces.Services
+{
+    public interface IPageIncludesService
+    {
+        PageAttributesViewModel GetPageAttributesViewModel(IPageSpecificIncludesComposition pageSpecificIncludesComposition);
+
+        string GetHeadIncludes(IGlobalIncludesComposition globalIncludes, IPageSpecificIncludesComposition pageSpecificIncludes);
+
+        string GetBodyIncludes(IGlobalIncludesComposition globalIncludes, IPageSpecificIncludesComposition pageSpecificIncludes); 
+    }
+}
