6f438ff390c370cfe1b9a4bc656900b678e92b96
Neil Hodges <nhodges@carbonsix.digital>
2022-03-08 16:06:31 +0000

    - Add an option to add a third-list level to the table of contents - Remove A to Z list - Add Related Links section underneath Page Meta Information

--- src/JNCC.PublicWebsite.Core/ViewModels/ScienceCategorySubSectionViewModel.cs
+++ src/JNCC.PublicWebsite.Core/ViewModels/ScienceCategorySubSectionViewModel.cs
@@ -1,6 +1,9 @@
-﻿namespace JNCC.PublicWebsite.Core.ViewModels
+﻿using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
 {
     public abstract class ScienceCategorySubSectionViewModel : ScienceCategorySectionViewModelBase
     {
+        public IEnumerable<ScienceCategorySubSectionViewModel> SubSections { get; set; }
     }
 }
