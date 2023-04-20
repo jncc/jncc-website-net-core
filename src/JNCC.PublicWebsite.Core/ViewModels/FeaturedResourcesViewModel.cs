515e3c21f54add9511086b4f70d7b04aa2db6f27
Tim <tim@attackmonkey.co.uk>
2023-04-20 15:20:13 +0100

    David's FE tweaks to make everything look as per current site.

--- src/JNCC.PublicWebsite.Core/ViewModels/FeaturedResourcesViewModel.cs
+++ src/JNCC.PublicWebsite.Core/ViewModels/FeaturedResourcesViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace JNCC.PublicWebsite.Core.ViewModels
 {
@@ -7,5 +9,13 @@ namespace JNCC.PublicWebsite.Core.ViewModels
         public string Title { get; set; }
 
         public IEnumerable<FeaturedResourceViewModel> FeaturedResources { get; set; } = new List<FeaturedResourceViewModel>();
+
+        public int ItemsPerColumn
+        {
+            get
+            {
+                return FeaturedResources.Count() > 0 ? (int)Math.Round((decimal)FeaturedResources.Count() / 2, MidpointRounding.AwayFromZero) : 0;
+            }
+        }
     }
 }
