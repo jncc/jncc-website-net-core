4e0844b6dc1bb2b1b4dd87d497465666dfc60692
Alex Wilks <awilks@carbonsix.digital>
2026-04-10 16:51:25 +0100

    Upgrade Nested Content to Block List

--- src/JNCC.PublicWebsite.Core/Interfaces/Services/ICategorisedFooterLinksService.cs
+++ src/JNCC.PublicWebsite.Core/Interfaces/Services/ICategorisedFooterLinksService.cs
@@ -1,10 +1,10 @@
-﻿using JNCC.PublicWebsite.Core.Models;
-using JNCC.PublicWebsite.Core.ViewModels;
+﻿using JNCC.PublicWebsite.Core.ViewModels;
+using Umbraco.Cms.Core.Models.Blocks;
 
 namespace JNCC.PublicWebsite.Core.Interfaces.Services
 {
     public interface ICategorisedFooterLinksService
     {
-        IEnumerable<CategorisedFooterLinksViewModel> GetViewModels(IEnumerable<CategorisedFooterLinksSchema> schemas);
+        IEnumerable<CategorisedFooterLinksViewModel> GetViewModels(BlockListModel schemas);
     }
 }
--- src/JNCC.PublicWebsite.Core/ViewModels/CarouselViewModel.cs
+++ src/JNCC.PublicWebsite.Core/ViewModels/CarouselViewModel.cs
@@ -4,8 +4,8 @@ namespace JNCC.PublicWebsite.Core.ViewModels
 {
     public sealed class CarouselViewModel
     {
-        public string Headline { get; set; }
-        public IHtmlEncodedString Text { get; set; }
-        public IEnumerable<ImageViewModel> Images { get; set; }
+        public string Headline { get; set; } = "";
+        public IHtmlEncodedString Text { get; set; } = new HtmlEncodedString("");
+        public IEnumerable<ImageViewModel> Images { get; set; } = new List<ImageViewModel>();
     }
 }
